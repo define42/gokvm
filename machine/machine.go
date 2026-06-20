4f3a175b630a7aa92e63c47570f7db5541039e4b
define42 <define42@github.com>
2026-06-20 11:30:45 +0200

    Removed unused code

--- machine/machine.go
+++ machine/machine.go
@@ -35,8 +35,6 @@ const (
 	initrdAddr  = 0xf000000
 	highMemBase = 0x100000
 	bootStack   = 0x80000
-	biosTop     = 0x100000
-	firmwareMax = 16 << 20
 
 	serialIRQ              = 4
 	ps2KeyboardIRQ         = 1
@@ -157,7 +155,6 @@ type Machine struct {
 	ioportHandlers [0x10000][2]func(port uint64, bytes []byte) error
 	stopped        uint32
 	vesaEnabled    bool
-	biosROM        []byte
 }
 
 // Close stops vCPU goroutines and releases PCI device
@@ -178,11 +175,6 @@ func (m *Machine) Close() error {
 	if m.kvmFile != nil {
 		_ = m.kvmFile.Close()
 		m.kvmFile = nil
-	}
-
-	if m.biosROM != nil {
-		_ = syscall.Munmap(m.biosROM)
-		m.biosROM = nil
 	}
 
 	return nil
@@ -518,63 +510,6 @@ func (m *Machine) LoadPVH(kern, initrd io.ReaderAt, cmdline string) error {
 	m.AddDevice(iodev.NewCMOS(uint64(len(m.mem)), 0x0))
 	m.AddDevice(iodev.NewACPIPMTimer())
 	m.initIOPortHandlers()
-
-	return nil
-}
-
-func (m *Machine) LoadBIOS(bios []byte) error {
-	if len(bios) == 0 {
-		return ErrZeroSizeKernel
-	}
-	if len(bios) > firmwareMax {
-		return fmt.Errorf("firmware image size %d:%w", len(bios), ErrUnsupported)
-	}
-
-	if len(bios) <= biosTop {
-		biosBase := biosTop - len(bios)
-		copy(m.mem[biosBase:biosTop], bios)
-	}
-
-	var err error
-	m.biosROM, err = syscall.Mmap(-1, 0, len(bios),
-		syscall.PROT_READ|syscall.PROT_WRITE,
-		syscall.MAP_SHARED|syscall.MAP_ANONYMOUS)
-	if err != nil {
-		return err
-	}
-	copy(m.biosROM, bios)
-
-	if err := kvm.SetUserMemoryRegion(m.vmFd, &kvm.UserspaceMemoryRegion{
-		Slot:          1,
-		Flags:         0,
-		GuestPhysAddr: uint64(0x1_0000_0000 - len(bios)),
-		MemorySize:    uint64(len(bios)),
-		UserspaceAddr: uint64(uintptr(unsafe.Pointer(&m.biosROM[0]))),
-	}); err != nil {
-		_ = syscall.Munmap(m.biosROM)
-		m.biosROM = nil
-
-		return err
-	}
-
-	for cpuID, cpu := range m.vcpuFds {
-		if err := m.initBIOSRegs(cpu, cpuID == 0); err != nil {
-			return err
-		}
-	}
-
-	if m.serial, err = serial.New(m); err != nil {
-		return err
-	}
-
-	m.AddDevice(&iodev.FWDebug{}) // Port 0x402
-	m.AddDevice(iodev.NewCMOS(uint64(len(m.mem)), 0x0))
-	m.AddDevice(iodev.NewFWCfg(uint64(len(m.mem)), uint16(len(m.vcpuFds)), "d"))
-	m.AddDevice(&iodev.Noop{Port: 0x80, Psize: 0xA0})
-	m.initIOPortHandlers()
-	if m.vesaEnabled {
-		m.installVESABIOS()
-	}
 
 	return nil
 }
@@ -951,60 +886,6 @@ func (m *Machine) initSregs(vcpufd uintptr, amd64 bool) error {
 	}
 
 	return nil
-}
-
-func (m *Machine) initBIOSRegs(vcpufd uintptr, bootstrap bool) error {
-	sregs, err := kvm.GetSregs(vcpufd)
-	if err != nil {
-		return err
-	}
-
-	sregs.CS = realModeSegment(0xf000, 0xffff0000, true)
-	sregs.DS = realModeSegment(0, 0, false)
-	sregs.ES = realModeSegment(0, 0, false)
-	sregs.FS = realModeSegment(0, 0, false)
-	sregs.GS = realModeSegment(0, 0, false)
-	sregs.SS = realModeSegment(0, 0, false)
-	sregs.IDT = kvm.Descriptor{Base: 0, Limit: 0x3ff}
-	sregs.GDT = kvm.Descriptor{Base: 0, Limit: 0}
-	sregs.CR0 = CR0xET
-	sregs.CR2 = 0
-	sregs.CR3 = 0
-	sregs.CR4 = 0
-	sregs.CR8 = 0
-	sregs.EFER = 0
-
-	if err := kvm.SetSregs(vcpufd, sregs); err != nil {
-		return err
-	}
-
-	regs, err := kvm.GetRegs(vcpufd)
-	if err != nil {
-		return err
-	}
-
-	*regs = kvm.Regs{RFLAGS: 2, RIP: 0xfff0}
-	if !bootstrap {
-		_ = kvm.SetMPState(vcpufd, &kvm.MPState{State: kvm.MPStateHalted})
-	}
-
-	return kvm.SetRegs(vcpufd, regs)
-}
-
-func realModeSegment(selector uint16, base uint64, code bool) kvm.Segment {
-	typ := uint8(3) // Data: read/write, accessed.
-	if code {
-		typ = 11 // Code: execute/read, accessed.
-	}
-
-	return kvm.Segment{
-		Base:     base,
-		Limit:    0xffff,
-		Selector: selector,
-		Typ:      typ,
-		Present:  1,
-		S:        1,
-	}
 }
 
 func (m *Machine) initCPUID(cpu int) error {
